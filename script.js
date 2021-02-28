696b69e38db82b7ccb620eea76aef8315fd66cd9
Sairaj9604 <70633841+Sairaj9604@users.noreply.github.com>
2021-02-28 12:34:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var options = {
+    bottom: '64px',
+    right: 'unset',
+    left: '32px',
+    time: '0.5s',
+    mixColor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    saveInCookies: false, 
+    label: '🌓',
+    autoMatchOsTheme: true,
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+AOS.init({
+    duration: 1500
+});
